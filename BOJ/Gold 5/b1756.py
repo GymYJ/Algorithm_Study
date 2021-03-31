2f01084fe53e35d81c0b584102787875d84c4637
GymYJ <digfox13@gmail.com>
2021-03-31 20:22:38 +0900

    STUDY: 2021-03-31 공부

--- BOJ/Gold 5/b1756.py
+++ BOJ/Gold 5/b1756.py
@@ -1,24 +1,34 @@
 import sys
 
-read = sys.stdin.readline
-inf = sys.maxsize
-
-D, N = map(int, read().split())
-oven = list(map(int, read().split()))
-pizza = list(map(int, read().split()))
-
-for i in range(1, D):
-    oven[i] = min(oven[i], oven[i - 1])
-
-pizzaIdx = 0
-depth = D - 1
-for i in reversed(range(D)):
-    if pizzaIdx >= len(pizza):
-        print(depth + 1)
-        sys.exit(0)
-
-    if oven[i] >= pizza[pizzaIdx]:
-        pizzaIdx += 1
-        depth = i
-
-print(0)
+d, n = map(int, sys.stdin.readline().split())
+oven = list(map(int, sys.stdin.readline().split()))
+pizza = list(map(int, sys.stdin.readline().split()))
+new_oven = [oven[0]]
+mini = oven[0]
+for i in range(1, d):
+    mini = min(mini, oven[i])
+    new_oven.append(mini)
+answer = 0
+left = 0
+right = d - 1
+check = True
+for i in range(len(pizza)):
+    if i != 0 and pizza[i] <= pizza[i - 1]:
+        answer = right + 1
+        right -= 1
+    else:
+        if right < 0 or pizza[i] > new_oven[0]:
+            answer = 0
+            break
+        index = 0
+        while left <= right:
+            mid = (left + right) // 2
+            if new_oven[mid] < pizza[i]:
+                right = mid - 1
+            else:
+                index = mid
+                left = mid + 1
+        answer = index + 1
+        right = index - 1
+        left = 0
+print(answer)
