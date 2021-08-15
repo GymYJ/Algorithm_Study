d672d0837e61c6ff330a6c2ecec6e07a985510a6
GymYJ <digfox13@gmail.com>
2021-08-15 15:23:29 +0900

    STUDY: 백준 1695번

---
+++ BOJ/Gold 4/b1695.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+dp = [[-1 for _ in range(n)] for _ in range(n)]
+for length in range(n):
+    for start in range(n):
+        end = start + length
+        if end >= n:
+            break
+        if start == end:
+            dp[start][end] = 0
+            continue
+        elif start + 1 == end:
+            if arr[start] == arr[end]:
+                dp[start][end] = 0
+            else:
+                dp[start][end] = 1
+            continue
+        else:
+            if arr[start] == arr[end]:
+                dp[start][end] = dp[start + 1][end - 1]
+            else:
+                dp[start][end] = min(dp[start][end - 1] + 1, dp[start + 1][end] + 1)
+print(dp[0][n - 1])
